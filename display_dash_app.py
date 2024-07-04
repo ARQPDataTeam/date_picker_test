f9d7b0b62270a38aad387513416fc38b84f8b6dc
bernski <bernard.firanski@ec.gc.ca>
2024-07-04 11:19:30 -0400

    added temp as second axis

--- display_dash_app.py
+++ display_dash_app.py
@@ -1,4 +1,5 @@
-from dash import Dash, html, dcc 
+import dash
+from dash import Dash, html, dcc, callback 
 from dash.exceptions import PreventUpdate
 import plotly.express as px
 import plotly.graph_objects as go
@@ -13,8 +14,8 @@ from azure.keyvault.secrets import SecretClient
 import os
 from dotenv import load_dotenv 
 
-# initialize the dash app as 'app'
-app = Dash(__name__)
+# register this as a page in the app
+dash.register_page(__name__)
 
 # set a try except clause to grab the online credentials keys and if not, grab them locally as environment variables
 try:
@@ -48,7 +49,7 @@ except Exception as e:
     print ('Credentials loaded locally')
 
 # set the sql engine string
-sql_engine_string=('postgresql://{}:{}@{}/{}?sslmode=require').format(DB_USER,DB_PASS,DB_HOST,DB_NAME)
+sql_engine_string=('postgresql://{}:{}@{}/{}').format(DB_USER,DB_PASS,DB_HOST,DB_NAME)
 print ('sql engine string: ',sql_engine_string)
 sql_engine=create_engine(sql_engine_string)
 
@@ -62,16 +63,18 @@ SELECT DISTINCT ON (datetime) * FROM (
 	WHERE ws_u IS NOT NULL
 	AND datetime >= '2024-03-01' AND datetime < '2024-03-01 01:00:00'
 ) AS csat
-ORDER BY datetime;    """
+ORDER BY datetime;
+"""
 
 # create the dataframe from the sql query
-met_output_df=pd.read_sql_query(sql_query, con=sql_engine)
-print (met_output_df)
+csat_output_df=pd.read_sql_query(sql_query, con=sql_engine)
 
-met_output_df.set_index('datetime', inplace=True)
-met_output_df.index=pd.to_datetime(met_output_df.index)
-beginning_date=met_output_df.index[0]
-ending_date=met_output_df.index[-1]
+# print (csat_output_df)
+
+csat_output_df.set_index('datetime', inplace=True)
+csat_output_df.index=pd.to_datetime(csat_output_df.index)
+beginning_date=csat_output_df.index[0]
+ending_date=csat_output_df.index[-1]
 today=dt.today().strftime('%Y-%m-%d')
 print(beginning_date, ending_date)
 # use specs parameter in make_subplots function
@@ -80,24 +83,28 @@ print(beginning_date, ending_date)
 
 # plot a scatter chart by specifying the x and y values
 # Use add_trace function to specify secondary_y axes.
-def create_figure(met_output_df):
+def create_figure(csat_output_df):
     fig = make_subplots(specs=[[{"secondary_y": True}]])
     fig.add_trace(
-        go.Scatter(x=met_output_df.index, y=met_output_df['u'], name="U WInd Speed"),
+        go.Scatter(x=csat_output_df.index, y=csat_output_df['u'], name="U Wind Speed"),
         secondary_y=False)
     
     # Use add_trace function and specify secondary_y axes = True.
     fig.add_trace(
-        go.Scatter(x=met_output_df.index, y=met_output_df['v'], name="V Wind Speed"),
+        go.Scatter(x=csat_output_df.index, y=csat_output_df['v'], name="V Wind Speed"),
+        secondary_y=False,)
+
+    fig.add_trace(
+        go.Scatter(x=csat_output_df.index, y=csat_output_df['vtemp'], name="Virt Temp"),
         secondary_y=True,)
 
     # set axis titles
     fig.update_layout(
         template='simple_white',
-        title='HWY 401 CSAT Data',
+        title='CRU CSAT Data',
         xaxis_title="Date",
-        yaxis_title="WS U",
-        yaxis2_title="WS V",
+        yaxis_title="Winds (m/s)",
+        yaxis2_title="Virt Temp (C)",
         legend=dict(
         yanchor="top",
         y=0.99,
@@ -108,17 +115,17 @@ def create_figure(met_output_df):
     return fig
 
 # set up the app layout
-app.layout = html.Div(children=
+layout = html.Div(children=
                     [
-                    html.H1(children=['SWAPIT HWY 401 Met Dashboard']),
-                    html.Div(children=['Met plot display with date picker']),
+                    html.H1(children=['SWAPIT Cruiser CSAT Dashboard']),
+                    html.Div(children=['CSAT Met plot display with date picker']),
 
                     dcc.DatePickerRange(
                         id='my-date-picker-range',
                         min_date_allowed=beginning_date,
                         max_date_allowed=ending_date
                     ),
-                    dcc.Graph(id='hwy401-csat-plot',figure=create_figure(met_output_df)),
+                    dcc.Graph(id='cru-csat-plot',figure=create_figure(csat_output_df)),
                     
                     ] 
                     )
@@ -129,8 +136,8 @@ app.layout = html.Div(children=
 #     Input('date-picker', 'end_date')],
 #     [State('submit_button', 'n_clicks')])
 
-@app.callback(
-    Output('hwy401-csat-plot', 'figure'),
+@callback(
+    Output('cru-csat-plot', 'figure'),
     Input('my-date-picker-range', 'start_date'),
     Input('my-date-picker-range', 'end_date'))
 
@@ -139,11 +146,11 @@ def update_output(start_date, end_date):
     if not start_date or not end_date:
         raise PreventUpdate
     else:
-        output_selected_df = met_output_df.loc[
-            (met_output_df.index >= start_date) & (met_output_df.index <= end_date), :
+        output_selected_df = csat_output_df.loc[
+            (csat_output_df.index >= start_date) & (csat_output_df.index <= end_date), :
         ]
         return create_figure(output_selected_df)
 
 
-if __name__=='__main__':
-    app.run(debug=True)
+# if __name__=='__main__':
+#     app.run(debug=True)
